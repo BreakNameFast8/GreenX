05258e5cdf84e9a41ac7fd2248ce579b7629eecf
breakfast <nguyentaonelp0@gmail.com>
2024-10-10 21:50:34 +0700

    Create LuaprhBugs.lua

---
+++ LuaprhBugs.lua
@@ -0,0 +1,13 @@
+--leaked by fiji and mirrored
+
+-- // Luraph Marco 
+if not LPH_OBFUSCATED then
+    LPH_JIT = function(...) return ... end
+    LPH_JIT_MAX = function(...) return ... end
+    LPH_JIT_ULTRA = function(...) return ... end
+    LPH_NO_VIRTUALIZE = function(...) return ... end
+    LPH_NO_UPVALUES = function(f) return(function(...) return f(...) end) end
+    LPH_ENCSTR = function(...) return ... end
+    LPH_HOOK_FIX = function(...) return ... end
+    LPH_CRASH = function() return print(debug.traceback()) end
+end;
